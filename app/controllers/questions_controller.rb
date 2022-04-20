eea80d007bc29483fab692f2562e05e9c430f913
Поликарпов Валентин Вадимович (ЗК-0000879) <valentin.polikarpov@bia-tech.ru>
2022-04-20 11:40:34 +0300

    Lesson 3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,40 @@
 class QuestionsController < ApplicationController
+  def edit
+    @question = Question.find_by id: params[:id]
+  end
+
+  def update
+    @question = Question.find_by id: params[:id]
+    if @question.update question_params
+      redirect_to questions_path
+    else
+      # render :edit
+      redirect_to edit_question_path(params[:id])
+    end
+  end
+
   def index
     @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    # render plain: questions_path
+    @question = Question.new question_params
+    if @question.save
+      redirect_to questions_path
+    else
+      # render :new
+      redirect_to new_question_path
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/questions', to: 'questions#index'
+
+  # get '/questions', to: 'questions#index'
+  # get '/questions/new', to: 'questions#new'
+  # get '/questions/:id/edit', to: 'questions#edit'
+  # post '/questions', to: 'questions#create'
+  resources :questions, only: %i[index new edit create update]
 
   root 'pages#index'
 end
